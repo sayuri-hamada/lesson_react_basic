615d191235e1753697121beae799fbabe9218a2d
Sayuri Hamada <110365626+sayuri-hamada@users.noreply.github.com>
2022-08-09 11:53:30 +0900

    再レンダリングと副作用を知る(useEffect)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
+
+  //stateが変更されると再レンダリングされる
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  //引数にとった配列内の値の変更の場合にのみ実行される
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
 
   return (
     <>
@@ -13,7 +34,10 @@ const App = () => {
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>o(｀ω´ )o</p>}
     </>
   );
 };
